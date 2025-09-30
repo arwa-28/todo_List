54249a9db3f01ea0657b08600cf86e81f638ca11
arwa <arwam4307@gmail.com>
2025-09-30 05:35:42 +0300

    Implement user authentication (login, signup, logout)

--- todo_List/tasks/urls.py
+++ todo_List/tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
 
-
 urlpatterns = [
-    
+    path('tasks_list/', views.tasks_list, name='tasks_list'),
+    path('task/create/', views.task_create, name='task_create'),
+    path('task/<int:pk>/update/', views.task_update, name='task_update'),
+    path('task/<int:pk>/delete/', views.task_delete, name='task_delete'),
 ]
--- todo_List/todo_List/settings.py
+++ todo_List/todo_List/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tasks',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+LOGIN_REDIRECT_URL = '/tasks_list/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
